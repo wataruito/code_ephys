c27b8428a25ce962bd35dabd76e59605d20e5cc9
Wataru Ito <wataru.ito@gmail.com>
2018-01-05 19:10:41 -0500

    Add functionality to generate a table of time stamp and video frame number.

--- intan2neurosuit.m
+++ intan2neurosuit.m
@@ -5,12 +5,12 @@ function intan2neurosuit
 %  the format readable by Neurosuit.
 %
 %  USAGE
-%    (Inside the directory where Intan files exit) 
+%    (Inside the directory where Intan files exit)
 %    >> intan2neurosuit
 %
 %  INPUTS
 %
-%    channels(required) -must be first input, numeric  
+%    channels(required) -must be first input, numeric
 %                        list of channels to load (use keyword 'all' for all)
 %                        channID is 0-indexing, a la neuroscope
 %  Name-value paired inputs:
@@ -20,7 +20,7 @@ function intan2neurosuit
 %                           whateverPath/baseName
 %                           and contain file baseName.lfp
 %    basename           -base file name to load
-%    intervals          -list of time intervals [0 10; 20 30] to read from 
+%    intervals          -list of time intervals [0 10; 20 30] to read from
 %                           the LFP file (default is [0 inf])
 %
 %  OUTPUT
@@ -55,7 +55,7 @@ function intan2neurosuit
 %   indexing.  This should no longer be necessary with .lfp.mat structs
 %
 % TODO
-% add saveMat input 
+% add saveMat input
 % expand channel selection options (i.e. region or spikegroup)
 % add forcereload
 
@@ -86,17 +86,18 @@ for k = 1:dataSeriseFilesNumber
         [intFileNameExt,t,amps,data,aux] = ...
             read_intan_data_2([pwd filesep dataSeriseFiles(k).name]);
         
-        
+        frame_convertor(intFileNameExt,t,aux);
         
         from_int_file_to_atf_time_trigger_files(intFileNameExt,t,amps,data,aux);
     end
+    disp(['completed ', num2str(k), '/', num2str(dataSeriseFilesNumber)]);
 end
 
 function [filename,t,amps,data,aux] = read_intan_data_2(filename)
 
 % [t,amps,data,aux] = read_intan_data
 %
-% Opens file selection GUI to select and then read data from an Intan 
+% Opens file selection GUI to select and then read data from an Intan
 % amplifier data file (*.int).
 %
 % t = time vector (in seconds)
@@ -154,7 +155,7 @@ end
 % Now search for the end of the file to find out the length of the data.
 % t_count = 0;
 % while (~feof(fid))
-%    fread(fid, 1+4*num_amps, 'uint8'); 
+%    fread(fid, 1+4*num_amps, 'uint8');
 %    t_count = t_count + 1;
 % end
 % t_count = t_count - 1;
@@ -241,19 +242,19 @@ fprintf(1, 'Completed!\n');
 
 % % Go back to the beginning of the file...
 % frewind(fid);
-% 
+%
 % % ...skip the header this time...
 % fread(fid, 3+64, 'uint8');
-% 
+%
 % % ...and read all the data.
 % fprintf(1, 'Reading data...  (This may take a while.)\n\n');
 % for i=1:t_count
 %     for j=1:num_amps
 %         data(i,j) = double(fread(fid, 1, 'float32'));
 %     end
-%     
+%
 %     aux_byte = fread(fid, 1, 'uint8');
-%     
+%
 %     % Decode auxiliary TTL inputs
 %     if aux_byte >= 32
 %         aux(i,6) = 1;
@@ -278,12 +279,11 @@ fprintf(1, 'Completed!\n');
 %     if aux_byte >= 1
 %         aux(i,1) = 1;
 %         aux_byte = aux_byte - 1;
-%     end        
+%     end
 % end
 
 % Close file, and we're done.
 fclose(fid);
-
 
 function from_int_file_to_atf_time_trigger_files(intFileNameExt,t,amps,data,aux)
 % Sweep length calculation
@@ -341,7 +341,7 @@ auxSeriseForSweep = zeros(channel_n ,sample_n, 6, sweep_n(1),'uint8');
 % Here channel 1,2 are right side (j=1), 3,4 are left (j=2).
 % When recording configuration is right amyhgala #5 #6 and left amygdala #11 #12
 % channels = [1 2; 3 4];
-% (2017-12-13 wi) When recording configuration is right amyhgala #5 #6 and right PL #8 
+% (2017-12-13 wi) When recording configuration is right amyhgala #5 #6 and right PL #8
 %                                   left PL #9 and left amygdala #11 #12
 channels = [1 2 3; 4 5 6];
 %channels = [4 5 6; 1 2 3];
@@ -382,3 +382,88 @@ end
 % Move the original int file to intFileName folder
 % movefile(intFileNameExt, intFileName);
 % exit;
+
+function frame_convertor(intFileNameExt,t,aux)
+
+% Read total number of sampling points, d1.
+[d1, ~] = size(aux);
+
+% Generating starting time (s) from filename
+[pathstr, fileNameNoExt, ~] = fileparts(intFileNameExt);
+[~, charLen] = size(fileNameNoExt);
+[~, ~, ~, HH, MM, SS] = datevec(fileNameNoExt(charLen - 5 : charLen), 'HHMMSS');
+startTime = SS + (MM + HH * 60) * 60;
+
+% Initializing variables
+bitNum = 27;
+auxCh6 = 1; % pulse trains for timing
+auxCh5 = 2; % actual pulse input from master camera
+
+% Start scanning from begining
+ind = 1;
+pulseEndTs=t(ind);	% Time stamp at the end of a pulse
+pulseCount = 0;	% Counting pulses inside a train
+frameCount = 1;	% Counting trains (frames)
+pulse = 0;		% Bit is high(1) or low(0)
+
+while ind <= d1
+    if aux(ind,auxCh6) == 1 % Bit is high
+        if pulse==0 % Change from low to high
+            pulse=1; % pulse on state
+            
+            % Examine the detected pulse belongs to the same or previous
+            % train
+            % Intra-tain pulse
+            if t(ind)-pulseEndTs < 0.0002
+                % Should be intra train, so that puseCount 1 to 26
+                if pulseCount >= bitNum
+                    % Error process
+                end
+            end
+            
+            % Inter-train pulse
+            if t(ind)-pulseEndTs > 0.06
+                % Should be inter train
+                if pulseCount ~= 0
+                    % The last train has less than 27 pulses
+                    pulseCount = 0; % reset pulseCount
+                end
+            end
+            
+            if pulseCount == 0
+                %                timeStamp(frameCount) = t(ind) + startTime;
+                timeStamp(frameCount) = t(ind);
+                
+            end
+            
+            pulseCount = pulseCount + 1;
+            bitCh5(pulseCount) = 0; % initialize bitCh5
+            
+        elseif pulse == 1 % Continue high
+        end
+        
+        % Check Ch5
+        if aux(ind,auxCh5) == 1
+            bitCh5(pulseCount) = 1;
+        end
+        
+    elseif aux(ind,auxCh6) == 0 % Bit is low
+        if pulse == 1 % Change from high to low. End of one pulse
+            pulse = 0;
+            pulseEndTs = t(ind);
+            if pulseCount == bitNum
+                pulseCount = 0;	% reset pulseCount
+                % Process Ch5
+                frameNum(frameCount) = double(bi2de(fliplr(bitCh5)));
+                frameCount = frameCount + 1;
+            end
+        elseif pulse == 0 % Continue low
+        end
+    end
+    ind = ind + 1;
+end
+
+timeFrame = [timeStamp' frameNum'];
+file_name = [pathstr filesep fileNameNoExt '_timeFrame'];
+save(file_name, 'timeFrame');
+
